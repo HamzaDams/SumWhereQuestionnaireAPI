64b90c57f131204bd4fb6e47ee9385818bd6f994
HamzaDams <hddesignx@gmail.com>
2021-01-06 11:32:19 +0100

    Refactor & fix templates thymeleaf

--- src/main/java/fr/sumwhere/questionnaire/controller/QuestionnaireController.java
+++ src/main/java/fr/sumwhere/questionnaire/controller/QuestionnaireController.java
@@ -1,5 +1,6 @@
 package fr.sumwhere.questionnaire.controller;
 
+import fr.sumwhere.questionnaire.exception.BusinessResourceException;
 import fr.sumwhere.questionnaire.model.Questionnaire;
 import fr.sumwhere.questionnaire.service.QuestionnaireService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import javax.mail.MessagingException;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -16,13 +19,11 @@ public class QuestionnaireController {
    @Autowired
    private QuestionnaireService questionnaireService;
 
-    @PostMapping("/envoyer")
-    public ResponseEntity<?> envoyerEmail(@RequestBody Questionnaire request) {
-        System.out.println(request);
-        boolean isEnvoyer = this.questionnaireService.envoyerEmail(request.getEmailTo(),request.getSujet(),request.getDescription(),request.getEmail(),request.getAdresse(),request.getNom(),request.getPrenom(),request.getSite(), request.getLatitude(), request.getLongitude());
-        if(isEnvoyer) {
-
-            return new ResponseEntity<>(questionnaireService.sauvegarderQuestionnaire(request), HttpStatus.CREATED);
+    @PostMapping(value = "/email")
+    @ResponseBody
+    public ResponseEntity<?> sendMail(@RequestBody Questionnaire q) throws BusinessResourceException, MessagingException {
+        if(questionnaireService.envoyerQuestionnaire(q)){
+            return new ResponseEntity<>(questionnaireService.sauvegarderQuestionnaire(q), HttpStatus.CREATED);
         }
         else {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
--- src/main/java/fr/sumwhere/questionnaire/service/QuestionnaireService.java
+++ src/main/java/fr/sumwhere/questionnaire/service/QuestionnaireService.java
@@ -2,15 +2,16 @@ package fr.sumwhere.questionnaire.service;
 
 import fr.sumwhere.questionnaire.exception.BusinessResourceException;
 import fr.sumwhere.questionnaire.model.Questionnaire;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.mail.MessagingException;
 
 @Service
 @Transactional
 public interface QuestionnaireService {
 
-    boolean envoyerEmail(String to, String sujet, String texte, String email, String adresse, String nom, String prenom, String site, float latitude, float longitude) throws BusinessResourceException;
+    Boolean envoyerQuestionnaire(Questionnaire q) throws MessagingException;
     Questionnaire sauvegarderQuestionnaire(Questionnaire q) throws  BusinessResourceException;
 
 }
